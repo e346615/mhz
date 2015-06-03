6464a1722cb8bcaea68cb62c144efd705ade2e72
chipsnsalsa2000 <chipsnsalsa2000@gmamil.com>
2015-06-03 15:40:31 -0400

    works with Tesseract

---
+++ PdfReader1/Form1.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.od = New System.Windows.Forms.OpenFileDialog()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.FilenameBox = New System.Windows.Forms.TextBox()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.SuspendLayout()
+        '
+        'od
+        '
+        Me.od.FileName = "OpenFileDialog1"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(36, 30)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(140, 28)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Open PDF file"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Location = New System.Drawing.Point(0, 79)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(981, 468)
+        Me.TextBox1.TabIndex = 1
+        '
+        'FilenameBox
+        '
+        Me.FilenameBox.Location = New System.Drawing.Point(235, 30)
+        Me.FilenameBox.Margin = New System.Windows.Forms.Padding(4)
+        Me.FilenameBox.Name = "FilenameBox"
+        Me.FilenameBox.ReadOnly = True
+        Me.FilenameBox.Size = New System.Drawing.Size(648, 22)
+        Me.FilenameBox.TabIndex = 2
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(980, 545)
+        Me.Controls.Add(Me.FilenameBox)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "MainForm"
+        Me.Text = "Murray's PDF File Reader"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents od As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents FilenameBox As System.Windows.Forms.TextBox
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+
+End Class
---
+++ PdfReader1/Form1.vb
@@ -0,0 +1,91 @@
+﻿Option Explicit On
+
+Imports System.Data.OleDb
+Imports Spire.Pdf
+Imports Spire.Pdf.Actions
+Imports Spire.Pdf.Annotations
+Imports Spire.Pdf.General
+Imports Spire.Pdf.Graphics
+Imports Spire.Pdf.Tables
+Imports System.IO
+Imports System.Text
+Imports Tesseract
+Imports System.Xml
+Imports VBScript_RegExp_55
+
+Public Class MainForm
+
+    Dim doc As PdfDocument
+    Dim ListImage As List(Of Image)
+    Dim images() As Image
+    Dim te As New Tesseract.TesseractEngine("C:\Users\Chip\Documents\Visual Studio 2013\Projects\tesseract-master\Samples\Tesseract.ConsoleDemo", "eng")
+    Dim img As Image
+    Dim PatternFile As XmlDocument = New XmlDocument
+    Dim PatternList As XmlNodeList
+
+    Dim CompleteText As String
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        doc = New PdfDocument()
+        Dim PatternNode As XmlNode
+        TextBox1.Clear()
+        FilenameBox.Clear()
+        od.Reset()
+        od.ShowDialog()
+
+        FilenameBox.Text = Path.GetFileName(od.FileName)
+        FilenameBox.Update()
+
+        doc.LoadFromFile(od.FileName)
+
+        Dim buffer As StringBuilder = New StringBuilder
+
+        ' get the hard text
+        Dim pagenumber As Integer = 0
+        For Each page As PdfPageBase In doc.Pages
+
+            Dim itm As New ListViewItem
+            Dim ImageKey As String = Guid.NewGuid().ToString()
+
+            buffer.AppendLine("Page " & pagenumber + 1)
+            buffer.AppendLine("   <pure text>")
+            buffer.AppendLine(page.ExtractText())
+
+            ' get the image
+            img = doc.SaveAsImage(pagenumber)
+            Using pix = PixConverter.ToPix(img)
+                Using pp = te.Process(pix)
+                    buffer.AppendLine("   <OCR text>")
+                    buffer.AppendLine(pp.GetText())
+                    buffer.AppendLine("<confidence: " & 100 * pp.GetMeanConfidence() & "%>")
+                End Using
+            End Using
+            pagenumber = pagenumber + 1
+        Next page
+        doc.Close()
+        CompleteText = buffer.ToString
+
+        For Each PatternNode In PatternList
+            Dim re As New RegExp
+            Dim isMatch As Boolean
+            With re
+                .Pattern = PatternNode.InnerText
+                .Multiline = True
+                .Global = True
+                .IgnoreCase = True
+                isMatch = .Test(CompleteText)
+                buffer.AppendLine("Pattern [" & PatternNode.InnerText & "] match: " & isMatch)
+            End With
+        Next
+
+        TextBox1.Text = buffer.ToString ' & "(plus " & ImagePageList.Images.Count & " images)"
+
+        Me.Update()
+
+    End Sub
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        PatternFile.Load("C:\Users\Chip\Documents\Visual Studio 2013\Projects\PdfReader1\PdfReader1\XMLFile1.xml")
+        PatternList = PatternFile.SelectNodes("//windstorm/pattern")
+    End Sub
+End Class
